42c9fb2356ea41b925f35b5eda3cc9e76f2467dd
Kevin Pfab <kevin.pfab@gmail.com>
2017-12-21 18:44:58 -0500

    Initial ptr functions.

---
+++ ptr.go
@@ -0,0 +1,105 @@
+// Package ptr provides small helper functions for interacting with pointers of basic types safely and inline.
+// TODO: examples here
+package ptr
+
+import "time"
+
+// Bool returns a pointer to the input bool.
+func Bool(b bool) *bool {
+	return &b
+}
+
+// String returns a pointer to the input string.
+func String(s string) *string {
+	return &s
+}
+
+// Int returns a pointer to the input int.
+func Int(i int) *int {
+	return &i
+}
+
+// Int8 returns a pointer to the input int8.
+func Int8(i int8) *int8 {
+	return &i
+}
+
+// Int16 returns a pointer to the input int16.
+func Int16(i int16) *int16 {
+	return &i
+}
+
+// Int32 returns a pointer to the input int32.
+func Int32(i int32) *int32 {
+	return &i
+}
+
+// Int64 returns a pointer to the input int64
+func Int64(i int64) *int64 {
+	return &i
+}
+
+// Uint returns a pointer to the input uint.
+func Uint(u uint) *uint {
+	return &u
+}
+
+// Uint8 returns a pointer to the input uint8.
+func Uint8(u uint8) *uint8 {
+	return &u
+}
+
+// Uint16 returns a pointer to the input uint16.
+func Uint16(u uint16) *uint16 {
+	return &u
+}
+
+// Uint32 returns a pointer to the input uint32.
+func Uint32(u uint32) *uint32 {
+	return &u
+}
+
+// Uint64 returns a pointer to the input uint64.
+func Uint64(u uint64) *uint64 {
+	return &u
+}
+
+// Uintptr returns a pointer to the input uintptr.
+func Uintptr(u uintptr) *uintptr {
+	return &u
+}
+
+// Byte returns a pointer to the input byte.
+func Byte(b byte) *byte {
+	return &b
+}
+
+// Rune returns a pointer to the input rune.
+func Rune(r rune) *rune {
+	return &r
+}
+
+// Float32 returns a pointer to the input float32.
+func Float32(f float32) *float32 {
+	return &f
+}
+
+// Float64 returns a pointer to the input float64.
+func Float64(f float64) *float64 {
+	return &f
+}
+
+// Complex64 returns a pointer to the input complex64.
+func Complex64(c complex64) *complex64 {
+	return &c
+}
+
+// Complex128 returns a pointer to the input complex128.
+func Complex128(c complex128) *complex128 {
+	return &c
+}
+
+// Time returns a pointer to the input time.Time.
+func Time(t time.Time) *time.Time {
+	return &t
+}
---
+++ ptr_test.go
@@ -0,0 +1,250 @@
+package ptr
+
+import (
+	"testing"
+	"time"
+)
+
+func TestBool(t *testing.T) {
+	for _, b := range []bool{false, true} {
+		p := Bool(b)
+		if p == nil {
+			t.Fatalf("expected %t, got <nil>", b)
+		}
+		if b != *p {
+			t.Fatalf("expected %t, got %t", b, *p)
+		}
+	}
+}
+
+func TestString(t *testing.T) {
+	for _, s := range []string{"", "one", "fish", "two", "fish"} {
+		p := String(s)
+		if p == nil {
+			t.Fatalf("expected %q, got <nil>", s)
+		}
+		if s != *p {
+			t.Fatalf("expected %q, got %q", s, *p)
+		}
+	}
+}
+
+func TestInt(t *testing.T) {
+	for _, i := range []int{-1, 0, 1, 1, 2, 3, 5} {
+		p := Int(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestInt8(t *testing.T) {
+	for _, i := range []int8{-1, 0, 1, 1<<7 - 1, -1 << 7} {
+		p := Int8(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestInt16(t *testing.T) {
+	for _, i := range []int16{-1, 0, 1, 1<<15 - 1, -1 << 15} {
+		p := Int16(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestInt32(t *testing.T) {
+	for _, i := range []int32{-1, 0, 1, 1<<31 - 1, -1 << 31} {
+		p := Int32(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestInt64(t *testing.T) {
+	for _, i := range []int64{-1, 0, 1, 1<<63 - 1, -1 << 63} {
+		p := Int64(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestUint(t *testing.T) {
+	for _, i := range []uint{0, 1, 100, 1000} {
+		p := Uint(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestUint8(t *testing.T) {
+	for _, i := range []uint8{0, 1, 1<<8 - 1} {
+		p := Uint8(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestUint16(t *testing.T) {
+	for _, i := range []uint16{0, 1, 1<<16 - 1} {
+		p := Uint16(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestUint32(t *testing.T) {
+	for _, i := range []uint32{0, 1, 1<<32 - 1} {
+		p := Uint32(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestUint64(t *testing.T) {
+	for _, i := range []uint64{0, 1, 1<<64 - 1} {
+		p := Uint64(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestUintptr(t *testing.T) {
+	for _, i := range []uintptr{0, 1, 10, 100, 1000} {
+		p := Uintptr(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestByte(t *testing.T) {
+	for _, i := range []byte{0, 1, 1<<8 - 1} {
+		p := Byte(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestRune(t *testing.T) {
+	for _, i := range []rune{0, 1, 1<<16 - 1} {
+		p := Rune(i)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", i)
+		}
+		if i != *p {
+			t.Fatalf("expected %v, got %v", i, *p)
+		}
+	}
+}
+
+func TestFloat32(t *testing.T) {
+	for _, f := range []float32{-1.11, 0, 3.14159} {
+		p := Float32(f)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", f)
+		}
+		if f != *p {
+			t.Fatalf("expected %v, got %v", f, *p)
+		}
+	}
+}
+
+func TestFloat64(t *testing.T) {
+	for _, f := range []float64{-1.11, 0, 3.14159} {
+		p := Float64(f)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", f)
+		}
+		if f != *p {
+			t.Fatalf("expected %v, got %v", f, *p)
+		}
+	}
+}
+
+func TestComplex64(t *testing.T) {
+	for _, c := range []complex64{0, 1, 12i} {
+		p := Complex64(c)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", c)
+		}
+		if c != *p {
+			t.Fatalf("expected %v, got %v", c, *p)
+		}
+	}
+}
+
+func TestComplex128(t *testing.T) {
+	for _, c := range []complex128{0, 1, 500i} {
+		p := Complex128(c)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", c)
+		}
+		if c != *p {
+			t.Fatalf("expected %v, got %v", c, *p)
+		}
+	}
+}
+
+func TestTime(t *testing.T) {
+	for _, tm := range []time.Time{
+		time.Now(),
+		time.Now().Add(15 * time.Minute),
+		time.Now().Add(-40 * time.Hour),
+	} {
+		p := Time(tm)
+		if p == nil {
+			t.Fatalf("expected %v, got <nil>", tm)
+		}
+		if tm != *p {
+			t.Fatalf("expected %v, got %v", tm, *p)
+		}
+	}
+}
